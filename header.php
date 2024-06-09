d7daa951d27168f4d796036092f3b339d1a00d95
CamilleDreann <165321858+CamilleDreann@users.noreply.github.com>
2024-06-09 22:48:06 +0200

    Ajout des illustrations
    Ajout des illustration de personnage et de le début de quelques fichier

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil Confiz</title>
+</head>
+<body>
+
+    <header>
+        <h3>Confiz, la plateforme festive qui met la confiserie à portée de clic !</h3>
+        <img src="assets/images/bonbon.png" alt="personnage cartoon" title="personnage cartoon">
+        <img src="assets/images/banane.png" alt="personnage cartoon" title="personnage cartoon">
+    </header>
