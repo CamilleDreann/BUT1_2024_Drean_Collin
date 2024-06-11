4c97fc740c660bbe70153a88f7c0c63100f85139
Collin <ethan.collin2304@gmail.com>
2024-06-11 10:05:16 +0200

    🐛 Footer : Placement du svg Vague ✨ A propos : Réalisation de l'article à propos

--- apropos.php
+++ apropos.php
@@ -1,5 +1,16 @@
 <article>
 
+<div class="content-apropos">
 
+<div class="txt-apropos">
+    <h2><span class="span-vert">"</span>A PROPOS DE NOUS<span class="span-vert">"</span></h2>
+    <p>Jeff Beyoff, franco américain, arrivé à Paris pour des études de marketing, décide de créer en 2012 la société "Confiz", une centrale d'achat de confiseries. Camarade de Jack Dermitt, héritier de la société allemande "Haribo", il choisit d'exploiter le catalogue produit de la société de son ami pour offrir au réseau de magasins franchisés qu'il construit, la possibilité de gérer leur réapprovisionnement très simplement : en ligne !</p>
+</div>
+
+<div class="illustration-apropos">
+    <img src="./assets/images/fleur.png" alt="illustration de bonbon">
+</div>
+
+</div>
 
 </article>
