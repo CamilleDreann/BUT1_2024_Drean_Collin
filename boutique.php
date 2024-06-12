e19921a9603c44acc0da973e31f4225359e3a4a1
Collin <ethan.collin2304@gmail.com>
2024-06-12 11:19:34 +0200

    ✨ Ajout d'icon (sticker, icon) ⚡️ : Amélioration du login
    ✨ logout : création de la page logout
    ✨ Boutique : Création de la page boutique (en cours)

---
+++ boutique.php
@@ -0,0 +1,11 @@
+<?php
+$bgClass = "bg-boutique";
+include_once("head.php");
+include_once("db.php");
+?>
+
+<?php
+include_once("avis.php");
+include_once("apropos.php");
+include_once("footer.php");
+?>
--- connexion.php
+++ connexion.php
@@ -1,15 +1,24 @@
-<?php 
-/* $bgClass = "bg-index"; */
+<?php
+$bgClass = "bg-index";
 
-include_once("head.php");
+include_once ("head.php");
 ?>
 
-<div class="div-formConnexion">
-<form class="form-connexion" method="POST" action="login.php">
-  <div class="title-connexion">Bienvenu(e),<br><span class="grise">veuillez vous connectez pour continuer</span></div>
-  <input class="input" name="username" placeholder="username" type="text" required>
-  <input class="input" name="password" placeholder="Password" type="password" required>
-  <button class="button-confirm" type="submit">Connexion →</button>
-</form>
-</div>
 
+
+<div class="div-formConnexion">
+  <form class="form-connexion" method="POST" action="login.php">
+    <div class="title-connexion">Bienvenu(e),<br><span class="grise">veuillez vous connectez pour continuer</span></div>
+
+    <?php
+      if (isset($_SESSION["error"])) {
+        echo '<p class="error-message">' . $_SESSION["error"] . '</p>';
+        unset($_SESSION["error"]);
+      }
+      ?>
+
+    <input class="input" name="username" placeholder="Username" type="text" required>
+    <input class="input" name="password" placeholder="Password" type="password" required>
+    <button class="button-confirm" type="submit">Connexion →</button>
+  </form>
+</div>
