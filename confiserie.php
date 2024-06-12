d836407eb36851af1398050699bedeba01c12b47
CamilleDreann <165321858+CamilleDreann@users.noreply.github.com>
2024-06-12 11:46:25 +0200

    Modification de la base et ajout de l'animation sur la page confiserie
    ajout de l'animation à la confiserie et modif de la bdd

--- confiserie.php
+++ confiserie.php
@@ -1,6 +1,8 @@
 <?php
-include_once("header.php");
-include_once("db.php")
+
+$bgClass = "bg-confiserie";
+include_once("head.php");
+include_once("db.php");
 ?>
 
 <article class="confiserie">
@@ -31,6 +33,8 @@ include_once("db.php")
             <div class="blocConfiserie" style="background-color: var(<?php echo $currentColor; ?>);">
                 <img class="sachetbonbon" src="assets/images/bonbons/<?php echo $value['illustration']?>" alt="">
                 <h3 class="prixConfiserie"><?php echo $value['prix']?></h3>
+                <h3 class="nomBonbon"><?php echo $value['nom']?></h3>
+                <button class="panierConfiserie"><img src="assets/icon/ph_basket-bold.svg" alt="panier"></button>
             </div>
 
 
@@ -39,6 +43,30 @@ include_once("db.php")
         ?>
     </div>
 </article>
+<script> 
+
+let blocConfiserie = document.querySelectorAll(".blocConfiserie");
+
+blocConfiserie.forEach(element => {
+    let imagesachet = element.querySelector(".sachetbonbon");
+    let nomBonbon = element.querySelector(".nomBonbon");
+
+    element.addEventListener("mouseleave", function(event) {
+            nomBonbon.style.opacity = "0";
+            imagesachet.style.transform = "translateY(0)";
+            nomBonbon.style.transform = " translateY(-200px)";
+        });
+        element.addEventListener("mouseenter", function(event) {
+            imagesachet.style.transform = "rotate(-20deg) translateY(-110px)";
+            imagesachet.style.transition = "transform 0.2s ease-in-out";
+            nomBonbon.style.transform = " translateY(-160px) translateX(80px) rotate(-20deg)";
+            nomBonbon.style.transition = "opacity 0.18s ease-in-out, transform 0.4s ease-in-out";
+            nomBonbon.style.opacity = "1";
+        });
+
+});
+
+</script>
 <?php
 include_once("avis.php");
 include_once("apropos.php");
