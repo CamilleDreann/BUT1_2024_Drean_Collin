a339b042b1a0eddf0e765952246813b2bedaacd0
CamilleDreann <165321858+CamilleDreann@users.noreply.github.com>
2024-06-17 01:05:25 +0200

    Animation et reglage bug
    ajout du animation de loading et animation boutiques plus réglages du menu burger

--- header.php
+++ header.php
@@ -1,33 +1,177 @@
 <header>
 
-<div class="curvedTxt">
-<svg width="500" height="150">
-        <path id="curve-header" d="M 0 220 C 0 220, 250 -40 , 500 220"></path>
-        <text class="text-header" text-anchor="middle">
-            <textPath class="text__path" href="#curve-header" startOffset="50%">WE ARE CONFIZ</textPath>
-</text>
-    </svg>
-</div>
+<style>
+
+            /* Adapt path to font file and format depending on your build settings. */
+            
+            @font-face {
+                font-family: Copyduck;
+                src: url(path/to/font/file/Copyduck) format('woff2');
+                font-weight: normal;
+                font-style: normal;
+                font-display: swap;
+            }
+            
+            .warp__placeholder{
+                position: relative;
+                color: transparent;
+                font-size: .1px;
+            }
+            
+            .warp {
+                display: block;
+                position: relative;
+                width: 100%;
+                height: 100px;
+                font: normal 5rem Copyduck;
+                left : 350px;
+                top : -150px;
+                color : white;
+            }
+            
+            [class*='warp__'] {
+                display: block;
+                position: absolute;
+            }
+            
+            .warp__0 {
+                transform-origin: 50% 4.187rem;
+                transform: translate(17.203rem,19.847rem) rotate(-0.877576rad);
+            }
+            
+            .warp__1 {
+                transform-origin: 50% 4.187rem;
+                transform: translate(18.707rem,17.141rem) rotate(-0.744754rad);
+            }
+            
+            .warp__2 {
+                transform-origin: 50% 4.187rem;
+                transform: translate(22.612rem,14.712rem) rotate(-0.594743rad);
+            }
+            
+            .warp__3 {
+                transform-origin: 50% 4.187rem;
+                transform: translate(25.314rem,13.235rem) rotate(-0.477851rad);
+            }
+            
+            .warp__4 {
+                transform-origin: 50% 4.187rem;
+                transform: translate(27.697rem,11.974rem) rotate(-0.346033rad);
+            }
+            
+            .warp__5 {
+                transform-origin: 50% 4.187rem;
+                transform: translate(31.235rem,11.035rem) rotate(-0.194999rad);
+            }
+            
+            .warp__6 {
+                transform-origin: 50% 4.187rem;
+                transform: translate(34.595rem,10.632rem) rotate(-0.052736rad);
+            }
+            
+            .warp__7 {
+                transform-origin: 50% 4.187rem;
+                transform: translate(37.747rem,10.666rem) rotate(0.07571rad);
+            }
+            
+            .warp__8 {
+                transform-origin: 50% 4.187rem;
+                transform: translate(40.42rem,11.098rem) rotate(0.208417rad);
+            }
+            
+            .warp__9 {
+                transform-origin: 50% 4.187rem;
+                transform: translate(43.628rem,12.079rem) rotate(0.358659rad);
+            }
+            
+            .warp__10 {
+                transform-origin: 50% 4.187rem;
+                transform: translate(46.966rem,13.634rem) rotate(0.512041rad);
+            }
+            
+            .warp__11 {
+                transform-origin: 50% 4.187rem;
+                transform: translate(50.078rem,15.395rem) rotate(0.641123rad);
+            }
+            
+            .warp__12 {
+                transform-origin: 50% 4.187rem;
+                transform: translate(52.41rem,16.842rem) rotate(0.72885rad);
+            }
+            
+            .warp__13 {
+                transform-origin: 50% 4.187rem;
+                transform: translate(53.484rem,18.565rem) rotate(0.819516rad);
+            }
+            
+            .warp__14 {
+                transform-origin: 50% 4.187rem;
+                transform: translate(55.577rem,20.789rem) rotate(0.920933rad);
+            }
+        </style>        
+                
+        <div class="warp" aria-labelledby="warp-label">
+            <span id="warp-label" class="warp__placeholder">
+                 WE ARE CONFIZ 
+            </span>
+    
+            <span aria-role="presentation">
+                <span class="warp__0">
+                     
+                </span>
+                <span class="warp__1">
+                    W
+                </span>
+                <span class="warp__2">
+                    E
+                </span>
+                <span class="warp__3">
+                     
+                </span>
+                <span class="warp__4">
+                    A
+                </span>
+                <span class="warp__5">
+                    R
+                </span>
+                <span class="warp__6">
+                    E
+                </span>
+                <span class="warp__7">
+                     
+                </span>
+                <span class="warp__8">
+                    C
+                </span>
+                <span class="warp__9">
+                    O
+                </span>
+                <span class="warp__10">
+                    N
+                </span>
+                <span class="warp__11">
+                    F
+                </span>
+                <span class="warp__12">
+                    I
+                </span>
+                <span class="warp__13">
+                    Z
+                </span>
+                <span class="warp__14">
+                     
+                </span>
+            </span>
+        </div>
         
 
         <div class="txt-header">
-
-        <img src="assets/images/bonbon.png" alt="personnage cartoon" title="personnage cartoon" >
-
-        <p>Confiz, la plateforme festive qui met la confiserie à portée de clic !</p>
-
-        <img src="assets/images/banane.png" alt="personnage cartoon" title="personnage cartoon" >
-
+            <img src="assets/images/bonbon.png" alt="personnage cartoon" title="personnage cartoon" >
+            <p>Confiz, la plateforme festive qui met la confiserie à portée de clic !</p>
+            <img src="assets/images/banane.png" alt="personnage cartoon" title="personnage cartoon" >
         </div>
 
-        <div class="curvedTxt">
-        <svg width="500" height="250">
-        <path id="curve-header" d="M 0 220 C 0 220, 250 -40 , 500 220"></path>
-        <text class="text-header" text-anchor="middle">
-            <textPath class="text__path" href="#curve-header" startOffset="50%">AMUZ'TOI !</textPath>
-</text>
-    </svg>
-</div>
+        <h2 >AMUZ'TOI !</h2>
         
 </header>
 
